03d25b7694814aa12688067e785013de18523d79
Aman Kashyap <akashyap_be23@thapar.edu>
2025-10-31 07:03:12 +0000

    postcss build completed

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    "./index.html",
+    "./styles.html",
+    "./js/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
